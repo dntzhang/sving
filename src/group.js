446c5836df7e4b2b2582db3f800a8e1c889f51ca
张磊 <kmdjs@qq.com>
2015-06-23 10:16:38 +0800

    Hello , Sword!

---
+++ src/group.js
@@ -0,0 +1,48 @@
+﻿//group中可放入group
+
+    var Group = function () {
+        this.node = document.createElementNS(svgNS, "g");
+
+    }
+
+
+//{
+//    "arrow-end": "none",
+//    "arrow-start": "none",
+//    blur: 0,
+//    "clip-rect": "0 0 1e9 1e9",
+//    cursor: "default",
+//    cx: 0,
+//    cy: 0,
+//    fill: "#fff",
+//    "fill-opacity": 1,
+//    font: '10px "Arial"',
+//    "font-family": '"Arial"',
+//    "font-size": "10",
+//    "font-style": "normal",
+//    "font-weight": 400,
+//    gradient: 0,
+//    height: 0,
+//    href: "http://alloyteam.com/",
+//    "letter-spacing": 0,
+//    opacity: 1,
+//    path: "M0,0",
+//    r: 0,
+//    rx: 0,
+//    ry: 0,
+//    src: "",
+//    stroke: "#000",
+//    "stroke-dasharray": "",
+//    "stroke-linecap": "butt",
+//    "stroke-linejoin": "butt",
+//    "stroke-miterlimit": 0,
+//    "stroke-opacity": 1,
+//    "stroke-width": 1,
+//    target: "_blank",
+//    "text-anchor": "middle",
+//    title: "sword",
+//    transform: "",
+//    width: 0,
+//    x: 0,
+//    y: 0
+//}
